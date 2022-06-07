b221d6018833f34058b5b525705b56640c9203e5
Yusuf Akin <kutluhanakin@gmail.com>
2022-06-07 13:14:38 -0400

    final

--- src/main/java/com/example/springregistration/config/SecurityConfiguration.java
+++ src/main/java/com/example/springregistration/config/SecurityConfiguration.java
@@ -19,8 +19,23 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserService userService;
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider auth = new DaoAuthenticationProvider();
+        auth.setUserDetailsService(userService);
+        auth.setPasswordEncoder(passwordEncoder());
+        return auth;
+    }
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(authenticationProvider());
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
